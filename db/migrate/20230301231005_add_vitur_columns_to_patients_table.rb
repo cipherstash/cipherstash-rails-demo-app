7292475b2c6b1e632dfd7d2588dd6bdc906542f7
fimac <fiona@cipherstash.com>
2023-03-02 14:17:32 +1100

    includes migration for encrypted columns and update to model

---
+++ db/migrate/20230301231005_add_vitur_columns_to_patients_table.rb
@@ -0,0 +1,37 @@
+class AddViturColumnsToPatientsTable < ActiveRecord::Migration[7.0]
+  def change
+    add_column :patients, :__full_name_encrypted, :text
+    add_column :patients, :__full_name_match, :integer, limit: 2, array: true
+    add_column :patients, :__full_name_ore, :ore_64_8_v1
+
+    add_column :patients, :__email_encrypted, :text
+    add_column :patients, :__email_match, :integer, limit: 2, array: true
+    add_column :patients, :__email_ore, :ore_64_8_v1
+
+    add_column :patients, :__dob_encrypted, :text
+    add_column :patients, :__dob_ore, :ore_64_8_v1_term
+
+    add_column :patients, :__weight_encrypted, :text
+    add_column :patients, :__weight_ore, :ore_64_8_v1_term
+
+    add_column :patients, :__allergies_encrypted, :text
+    add_column :patients, :__allergies_match, :integer, limit: 2, array: true
+    add_column :patients, :__allergies_ore, :ore_64_8_v1
+
+    add_column :patients, :__medications_encrypted, :text
+    add_column :patients, :__medications_match, :integer, limit: 2, array: true
+    add_column :patients, :__medications_ore, :ore_64_8_v1
+
+    add_index :patients, :__full_name_ore
+    add_index :patients, :__email_ore
+    add_index :patients, :__dob_ore
+    add_index :patients, :__weight_ore
+    add_index :patients, :__allergies_ore
+    add_index :patients, :__medications_ore
+
+    add_index :patients, :__full_name_match, using: :gin
+    add_index :patients, :__email_match, using: :gin
+    add_index :patients, :__allergies_match, using: :gin
+    add_index :patients, :__medications_match, using: :gin
+  end
+end
